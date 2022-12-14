6d9620bb1b765e00cede72ea3db0505cc5cd229a
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2022-12-14 19:49:07 +0300

    feat: Project initial commit

---
+++ src/main/java/by/clevertec/ClevertecTestTaskApplication.java
@@ -0,0 +1,13 @@
+package by.clevertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClevertecTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClevertecTestTaskApplication.class, args);
+    }
+
+}
