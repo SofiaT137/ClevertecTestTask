ed7489308401bdcd1e67ea2f0091bbc1953219c4
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2022-12-14 22:23:03 +0300

    feat: Add controller classes

---
+++ src/main/java/by/clevertec/web/controller/CashReceiptController.java
@@ -0,0 +1,9 @@
+package by.clevertec.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("clevertecTestTask/v1/cashReceipt")
+public class CashReceiptController {
+}
