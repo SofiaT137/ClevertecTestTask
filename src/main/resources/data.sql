eda4371c5b14796e71c9a348bb9cee84b21b4e3c
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2022-12-14 20:29:24 +0300

    feat: Add data.sql file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,2 @@
 INSERT INTO discount_card(card_discount_percent) VALUES (5), (10), (20);
-INSERT INTO product(product_name,product_price,product_discount_percent)
-VALUES ('Strawberry Jam', 14.0, 0), ('Blueberry Jam', 25.12, 10), ('Marshmello', 5.60, 15);
+INSERT INTO product(product_name,product_price,product_discount_percent) VALUES ('Strawberry Jam', 14.0, 0), ('Blueberry Jam', 25.12, 10), ('Marshmello', 5.60, 15);
