44962f4aa2c9c4154fd8c53def56cba72db4906f
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2022-12-18 00:22:21 +0300

    feat: Add @Data annotation to the AbstractEntity class

--- src/main/java/by/clevertec/persistence/entity/AbstractEntity.java
+++ src/main/java/by/clevertec/persistence/entity/AbstractEntity.java
@@ -4,12 +4,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
 
 import java.io.Serializable;
 
 @MappedSuperclass
+@Data
 public abstract class AbstractEntity <K extends Serializable> {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private K id;
