ad9d59ad0bbe74bc6e623eb63a667e7d939b77df
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2022-12-18 22:50:16 +0300

    feat: Add parse text from file logic

--- src/main/java/by/clevertec/service/CashReceiptService.java
+++ src/main/java/by/clevertec/service/CashReceiptService.java
@@ -2,6 +2,10 @@ package by.clevertec.service;
 
 import org.springframework.util.MultiValueMap;
 
+import java.io.FileNotFoundException;
+
 public interface CashReceiptService<T>{
-    T getCashReceipt(MultiValueMap<String, String> mapWithParameters);
+    T getCashReceiptByTransferredParameters(MultiValueMap<String, String> mapWithParameters);
+
+    T getCashReceiptByFileData(String path) throws FileNotFoundException;
 }
