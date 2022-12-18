bcaffa46bb5f12ee8a023445a0bd930b91c132f5
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2022-12-18 22:52:01 +0300

    feat: Add @JsonFormat annotation to the cashReceitDate and cashReceiptTime fields

--- src/main/java/by/clevertec/service/dto/CashReceiptDto.java
+++ src/main/java/by/clevertec/service/dto/CashReceiptDto.java
@@ -1,5 +1,6 @@
 package by.clevertec.service.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
@@ -10,12 +11,18 @@ import java.util.List;
 @Data
 @Accessors(chain = true)
 public class CashReceiptDto {
+
+    private static final String DATE_REGEX = "yyyy/MM/dd";
+    private static final String TIME_REGEX = "HH:mm:ss";
+
     private CashierDto cashierDto;
     private ShopDto shopDto;
     private List<CashReceiptItemDto> cashReceiptItemList;
     private DiscountCardDto discountCardDto;
+    @JsonFormat(pattern = DATE_REGEX)
     private LocalDate cashReceiptDate;
+    @JsonFormat(pattern = TIME_REGEX)
     private LocalTime cashReceiptTime;
-    private Double totalDiscountPercent;
+    private Double totalDiscount;
     private Double totalPrice;
 }
