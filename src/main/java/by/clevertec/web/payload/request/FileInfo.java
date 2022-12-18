8ecc2163a40ce887bc92bcc58bb81713eff1a51a
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2022-12-18 22:54:37 +0300

    feat: Add FileInfo class

---
+++ src/main/java/by/clevertec/web/payload/request/FileInfo.java
@@ -0,0 +1,10 @@
+package by.clevertec.web.payload.request;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class FileInfo {
+    private String path;
+}
