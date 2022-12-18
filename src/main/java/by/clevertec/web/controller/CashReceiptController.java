cc13804e48f121369ff355e30240c788b4a9cdcf
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2022-12-18 22:59:41 +0300

    feat: Add new method getCashReceiptByFileData to the CashReceiptController class

--- src/main/java/by/clevertec/web/controller/CashReceiptController.java
+++ src/main/java/by/clevertec/web/controller/CashReceiptController.java
@@ -2,15 +2,19 @@ package by.clevertec.web.controller;
 
 import by.clevertec.service.CashReceiptService;
 import by.clevertec.service.dto.CashReceiptDto;
-import lombok.Getter;
+import by.clevertec.web.payload.request.FileInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.FileNotFoundException;
 
 @RestController
 @RequestMapping("clevertecTestTask/v1/cashReceipt")
@@ -19,9 +23,17 @@ public class CashReceiptController {
 
     private final CashReceiptService<CashReceiptDto> cashReceiptService;
     @GetMapping
-    public ResponseEntity<Object> giftCertificatesByParameter(@RequestParam MultiValueMap<String,
+    public ResponseEntity<Object> getCashReceiptByTransferredParameters(@RequestParam MultiValueMap<String,
             String> allRequestParams){
-         CashReceiptDto cashReceipt = cashReceiptService.getCashReceipt(allRequestParams);
+         CashReceiptDto cashReceipt = cashReceiptService.getCashReceiptByTransferredParameters(allRequestParams);
+        return new ResponseEntity<>(cashReceipt, HttpStatus.CREATED);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> getCashReceiptByFileData(@RequestBody FileInfo info)
+            throws FileNotFoundException {
+        System.out.println(info);
+        CashReceiptDto cashReceipt = cashReceiptService.getCashReceiptByFileData(info.getPath());
         return new ResponseEntity<>(cashReceipt, HttpStatus.CREATED);
     }
 }
