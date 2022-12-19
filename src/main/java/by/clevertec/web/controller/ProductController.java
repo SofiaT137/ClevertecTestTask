a4f212d1e4f6cf671024c1506524bfe02b26c55c
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2022-12-19 12:16:17 +0300

    feat: Add functionality to the Product controller class

--- src/main/java/by/clevertec/web/controller/ProductController.java
+++ src/main/java/by/clevertec/web/controller/ProductController.java
@@ -1,9 +1,49 @@
 package by.clevertec.web.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import by.clevertec.service.ProductService;
+import by.clevertec.service.dto.ProductDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("clevertecTestTask/v1/product")
+@RequiredArgsConstructor
 public class ProductController {
+    private final ProductService<ProductDto> productService;
+    @GetMapping
+    public ResponseEntity<Object> getAllProducts(@RequestParam(defaultValue = "0",required = false)
+                                                  int pageNumber,
+                                                  @RequestParam (defaultValue = "5", required = false)
+                                                  int pageSize){
+        Page<ProductDto> productDtoPage = productService.getAll(pageNumber,pageSize);
+        return new ResponseEntity<>(productDtoPage, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getProductById(@PathVariable Long id){
+        ProductDto productDto = productService.getById(id);
+        return new ResponseEntity<>(productDto, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> insertProduct(@RequestBody ProductDto productDto){
+        productService.insert(productDto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateProduct(@PathVariable Long id,
+                                                @RequestBody  ProductDto productDto){
+        productService.update(id,productDto);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable Long id){
+        productService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
