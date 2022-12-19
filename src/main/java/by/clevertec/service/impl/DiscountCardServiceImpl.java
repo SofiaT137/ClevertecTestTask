8a373a064a861e42ff8c90d97e970ce43b083f9f
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2022-12-19 11:50:48 +0300

    feat: Add logic ещ to the inherit internal methods

--- src/main/java/by/clevertec/service/impl/DiscountCardServiceImpl.java
+++ src/main/java/by/clevertec/service/impl/DiscountCardServiceImpl.java
@@ -1,39 +1,46 @@
 package by.clevertec.service.impl;
 
+import by.clevertec.persistence.entity.DiscountCard;
 import by.clevertec.persistence.repository.DiscountCardRepository;
 import by.clevertec.service.DiscountCardService;
 import by.clevertec.service.dto.DiscountCardDto;
 import by.clevertec.service.mapper.DiscountCardMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class DiscountCardServiceImpl implements DiscountCardService<DiscountCardDto> {
-
     private final DiscountCardRepository discountCardRepository;
-
     private final DiscountCardMapper discountCardMapper;
 
     @Override
     public void insert(DiscountCardDto discountCardDto) {
-        discountCardRepository.save(discountCardMapper.mapToDiscountCard(discountCardDto));
+        discountCardRepository
+                .save(discountCardMapper
+                        .mapToDiscountCard(discountCardDto));
     }
 
     @Override
     public DiscountCardDto getById(Long id) {
-        return discountCardMapper.mapToDiscountCardDto(discountCardRepository.findById(id).get());
+        return discountCardMapper
+                .mapToDiscountCardDto(discountCardRepository
+                        .findById(id)
+                        .orElseThrow(RuntimeException::new));
     }
 
     @Override
     public Page<DiscountCardDto> getAll(int pageNumber, int pageSize) {
-        return null;
+        Page<DiscountCard> discountCard = discountCardRepository.findAll(PageRequest.of(pageNumber, pageSize));
+        return discountCard.map(discountCardMapper::mapToDiscountCardDto);
     }
 
     @Override
-    public void update(Long id, DiscountCardDto dto) {
-
+    public void update(Long id, DiscountCardDto discountCardDto) {
+        DiscountCard discountCard = discountCardMapper.mapToDiscountCard(getById(id));
+        discountCardMapper.updateDiscountCardFromDiscountCardDto(discountCardDto, discountCard);
     }
 
     @Override
