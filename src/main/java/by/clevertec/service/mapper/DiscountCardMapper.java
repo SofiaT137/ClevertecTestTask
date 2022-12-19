022a917ca9ea5b2c8efac33ddfc8ea894c1658f3
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2022-12-19 11:40:03 +0300

    feat: Add updateDiscountCardFromDiscountCardDto method to the DiscountCardMapper class

--- src/main/java/by/clevertec/service/mapper/DiscountCardMapper.java
+++ src/main/java/by/clevertec/service/mapper/DiscountCardMapper.java
@@ -2,10 +2,16 @@ package by.clevertec.service.mapper;
 
 import by.clevertec.persistence.entity.DiscountCard;
 import by.clevertec.service.dto.DiscountCardDto;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring")
 public interface DiscountCardMapper {
     DiscountCard mapToDiscountCard(DiscountCardDto discountCardDto);
     DiscountCardDto mapToDiscountCardDto(DiscountCard discountCard);
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateDiscountCardFromDiscountCardDto(DiscountCardDto discountCardDto,
+                                               @MappingTarget DiscountCard discountCard);
 }
