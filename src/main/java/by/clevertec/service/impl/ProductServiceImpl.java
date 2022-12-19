b863096fa47d535e93a7f07909b8ef4b9a090eed
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2022-12-19 11:44:39 +0300

    feat: Add realization to the штеуinternal method

--- src/main/java/by/clevertec/service/impl/ProductServiceImpl.java
+++ src/main/java/by/clevertec/service/impl/ProductServiceImpl.java
@@ -1,2 +1,50 @@
-package by.clevertec.service.impl;public class ProductServiceImpl {
+package by.clevertec.service.impl;
+
+import by.clevertec.persistence.entity.Product;
+import by.clevertec.persistence.repository.ProductRepository;
+import by.clevertec.service.ProductService;
+import by.clevertec.service.dto.ProductDto;
+import by.clevertec.service.mapper.ProductMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProductServiceImpl implements ProductService<ProductDto> {
+
+    private final ProductRepository productRepository;
+    private final ProductMapper productMapper;
+
+    @Override
+    public void insert(ProductDto productDto) {
+        productRepository
+                .save(productMapper
+                        .mapToProduct(productDto));
+    }
+    @Override
+    public ProductDto getById(Long id) {
+        return productMapper
+                .mapToProductDto(productRepository
+                        .findById(id)
+                        .orElseThrow(RuntimeException::new));
+    }
+
+    @Override
+    public Page<ProductDto> getAll(int pageNumber, int pageSize) {
+        Page<Product> products = productRepository.findAll(PageRequest.of(pageNumber, pageSize));
+        return products.map(productMapper::mapToProductDto);
+    }
+
+    @Override
+    public void update(Long id, ProductDto productDto) {
+        Product product = productMapper.mapToProduct(getById(id));
+        productMapper.updateProductFromProductDto(productDto, product);
+    }
+
+    @Override
+    public void delete(Long id) {
+        productRepository.deleteById(id);
+    }
 }
