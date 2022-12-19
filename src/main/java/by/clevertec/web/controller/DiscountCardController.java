5cc24cc89b7ee28a99bb28f2311c37d2c4bc2229
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2022-12-19 12:24:24 +0300

    feat: Add functionality to the DiscountCardController class

--- src/main/java/by/clevertec/web/controller/DiscountCardController.java
+++ src/main/java/by/clevertec/web/controller/DiscountCardController.java
@@ -1,10 +1,51 @@
 package by.clevertec.web.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import by.clevertec.service.DiscountCardService;
+import by.clevertec.service.dto.DiscountCardDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("clevertecTestTask/v1/discountCard")
+@RequiredArgsConstructor
 public class DiscountCardController {
 
+    private final DiscountCardService<DiscountCardDto> discountCardService;
+
+    @GetMapping
+    public ResponseEntity<Object> getAllDiscountCards(@RequestParam(defaultValue = "0", required = false)
+                                                 int pageNumber,
+                                                 @RequestParam(defaultValue = "5", required = false)
+                                                 int pageSize) {
+        Page<DiscountCardDto> discountCardDtoPage = discountCardService.getAll(pageNumber, pageSize);
+        return new ResponseEntity<>(discountCardDtoPage, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getDiscountCardById(@PathVariable Long id) {
+        DiscountCardDto discountCardDto = discountCardService.getById(id);
+        return new ResponseEntity<>(discountCardDto, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> insertDiscountCard(@RequestBody DiscountCardDto discountCardDto) {
+        discountCardService.insert(discountCardDto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateDiscountCard(@PathVariable Long id,
+                                                @RequestBody DiscountCardDto discountCardDto) {
+        discountCardService.update(id, discountCardDto);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteDiscountCard(@PathVariable Long id) {
+        discountCardService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
