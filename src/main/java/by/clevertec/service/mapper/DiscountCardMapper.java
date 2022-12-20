09afcd6eca31378aedfc4d36d0129f318e94cedb
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2022-12-20 13:18:41 +0300

    feat: Add igore 'id' field to the methods

--- src/main/java/by/clevertec/service/mapper/DiscountCardMapper.java
+++ src/main/java/by/clevertec/service/mapper/DiscountCardMapper.java
@@ -4,14 +4,17 @@ import by.clevertec.persistence.entity.DiscountCard;
 import by.clevertec.service.dto.DiscountCardDto;
 import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring")
 public interface DiscountCardMapper {
+    @Mapping(target = "id", ignore = true)
     DiscountCard mapToDiscountCard(DiscountCardDto discountCardDto);
     DiscountCardDto mapToDiscountCardDto(DiscountCard discountCard);
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
     void updateDiscountCardFromDiscountCardDto(DiscountCardDto discountCardDto,
                                                @MappingTarget DiscountCard discountCard);
 }
--- src/main/java/by/clevertec/service/mapper/ProductMapper.java
+++ src/main/java/by/clevertec/service/mapper/ProductMapper.java
@@ -2,17 +2,16 @@ package by.clevertec.service.mapper;
 
 import by.clevertec.persistence.entity.Product;
 import by.clevertec.service.dto.ProductDto;
-import org.mapstruct.BeanMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
 
+    @Mapping(target = "id", ignore = true)
     Product mapToProduct(ProductDto productDto);
     ProductDto mapToProductDto(Product product);
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
     void updateProductFromProductDto(ProductDto productDto,
                                                @MappingTarget Product product);
 }
